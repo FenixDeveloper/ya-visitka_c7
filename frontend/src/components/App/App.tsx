85759a098318f91cb8566798d782391be13f85b9
SmthOfValue <bik00014@gmail.com>
2023-03-30 00:51:46 +0300

    init project/moved router inside App

--- frontend/src/components/App/App.tsx
+++ frontend/src/components/App/App.tsx
@@ -1,12 +1,17 @@
 import React, { FC } from 'react';
+import { RouterProvider } from 'react-router-dom';
+import { router } from '../../router/router';
 import AppStyles from './App.module.css';
 
 const App: FC = () => {
   return (
     <>
+      <header>Хедер</header>
       <main>
+        <RouterProvider router={router} />
 
       </main>
+      <footer>Футер</footer>
     </>
   );
 }
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,13 +4,14 @@ import './index.css';
 import reportWebVitals from './reportWebVitals';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './router/router';
+import App from './components/App/App';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-        <RouterProvider router={router} />
+        <App />
   </React.StrictMode>
 );
 
