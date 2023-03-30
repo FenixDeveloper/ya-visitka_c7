ce17a50c3036a02699698312921c524b441dd577
Karkachevich <karkachevichtv@yandex.ru>
2023-03-30 15:40:13 +0500

    feat: установил passport and passport-yandex

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,8 +1,34 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import passport from 'passport';
 import helmet from 'helmet';
 import { rateLimit } from 'express-rate-limit';
 import { PORT, DB_URL } from './config/config';
+
+const YandexStrategy = require('passport-yandex').Strategy;
+
+const YANDEX_CLIENT_ID = "--insert-yandex-client-id-here--"
+const YANDEX_CLIENT_SECRET = "--insert-yandex-client-secret-here--";
+
+passport.use(new YandexStrategy({
+  clientID: YANDEX_CLIENT_ID,
+  clientSecret: YANDEX_CLIENT_SECRET,
+  callbackURL: "http://127.0.0.1:3000/auth/yandex/callback"
+},
+// function(accessToken, refreshToken, profile, done) {
+//   User.find({ email: profile.email }, function (err, user) {
+//     return done(err, user);
+//   });
+// }
+));
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+  done(null, user as Express.User);
+});
 
 const limiter = rateLimit({
   windowMs: 16 * 60 * 1000,
@@ -13,6 +39,13 @@ const limiter = rateLimit({
 
 const app = express();
 mongoose.connect(DB_URL);
+
+app.get('/auth/yandex', passport.authenticate('yandex'));
+app.get('/auth/yandex/callback',
+  passport.authenticate('yandex', { failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/');
+  })
 
 app.use(limiter);
 app.use(helmet());
