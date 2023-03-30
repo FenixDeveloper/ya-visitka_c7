4dd7f97f131940d6a000d9b2e2cf15f6a3f9d5b7
Anton Aduisky <a.mltw@yandex.ru>
2023-03-30 19:00:37 +0300

    feat: header component is added

--- frontend/src/components/App/App.tsx
+++ frontend/src/components/App/App.tsx
@@ -1,16 +1,14 @@
-import React, { FC } from "react";
-import { RouterProvider } from "react-router-dom";
-import { router } from "../../router/router";
-import Footer from "../Footer/Footer";
-import Login from "../Login/Login";
-import { testUser } from "../../utils/constants";
+import React, { FC } from 'react';
+import { RouterProvider } from 'react-router-dom';
+import { router } from '../../router/router';
+import Footer from '../Footer/Footer';
+import Header from '../Header/Header';
+import { testUser } from '../../utils/constants';
 
 const App: FC = () => {
   return (
     <>
-      <header>
-        <Login user={testUser} />
-      </header>
+      <Header user={testUser} />
       <main>
         <RouterProvider router={router} />
       </main>
