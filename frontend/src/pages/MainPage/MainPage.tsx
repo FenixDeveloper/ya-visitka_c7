530b1eb03f33b8e2d7a6a8af9a95a40df100c8c1
SmthOfValue <bik00014@gmail.com>
2023-03-31 18:52:11 +0300

    added correct button colors and styles, small fix for Login page styles

--- frontend/src/pages/MainPage/MainPage.tsx
+++ frontend/src/pages/MainPage/MainPage.tsx
@@ -1,5 +1,4 @@
 import { FC } from "react";
-import { Button } from "../../components/Button/Button";
 import MainPageStyles from './MainPage.module.css';
 
 export const MainPage: FC = () => {
@@ -7,7 +6,6 @@ export const MainPage: FC = () => {
       <>
         <h1>
             Главная страница
-            <Button disabled onClick={() => console.log('нажалась кнопка')}>Text</Button>
         </h1>
       </>
     );
