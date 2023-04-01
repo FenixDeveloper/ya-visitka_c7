d14b763ac2785ad14eb32ac18b4062b8bffff7d6
Artrudjob <Artrudjob@yandex.ru>
2023-04-01 18:20:20 +0300

    feat: add PageLogin

--- frontend/src/pages/LoginPage/LoginPage.tsx
+++ frontend/src/pages/LoginPage/LoginPage.tsx
@@ -1,12 +1,31 @@
-import { FC } from "react";
-import LoginPageStyles from './LoginPage.module.css';
+import { FC, useEffect, useState } from "react";
+import { Button } from "../../components/Button/Button";
+import LoginPageStyles from './LoginPage.module.scss';
 
 export const LoginPage: FC = () => {
+
+    const [windowWidth, setWindowWidth] = useState<number>(window.innerWidth);
+
+    useEffect(() => {
+      function updateWindowWidth() {
+        setWindowWidth(window.innerWidth)
+      };
+
+      window.addEventListener('resize', updateWindowWidth);
+
+      return () => window.removeEventListener('resize', updateWindowWidth);
+    }, [windowWidth])
+
     return (
-      <>
-        <h1>
-            Страница авторизации
+      <section className={LoginPageStyles.loginPage}>
+        <h1 className={LoginPageStyles.loginPage__title}>
+            С кем я учусь?
         </h1>
-      </>
+        <div className={LoginPageStyles.loginPage__buttonBox}>
+          <Button size={windowWidth <= 576 ? 'medium' : 'large'} htmlType='button' onClick={() => {}}>
+            <p className={LoginPageStyles.loginPage__buttonText}>Войти с Яндекс ID</p>
+          </Button>
+        </div>
+      </section>
     );
   }
