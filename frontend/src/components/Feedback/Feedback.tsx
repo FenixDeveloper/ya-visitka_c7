d685f7a7ac4223e1f63cc0ea388a81354241ba8b
isvakulenko <ivan-vakulenko@yandex.ru>
2023-04-01 16:45:07 +0300

    Add feedback form with test text

---
+++ frontend/src/components/Feedback/Feedback.tsx
@@ -0,0 +1,72 @@
+import { FC, useState } from 'react';
+import likeIcon from '../../assets/icons/reactions/👍.svg';
+import dislikeIcon from '../../assets/icons/reactions/👎️.svg';
+import waveIcon from '../../assets/icons/reactions/👋️.svg';
+import smileIcon from '../../assets/icons/reactions/🙂️.svg';
+import sadIcon from '../../assets/icons/reactions/😞️.svg';
+import laughIcon from '../../assets/icons/reactions/🤣️.svg';
+import angryIcon from '../../assets/icons/reactions/😬️.svg';
+import scaryIcon from '../../assets/icons/reactions/😱️.svg';
+import heartIcon from '../../assets/icons/reactions/❤️.svg';
+import styles from './Feedback.module.scss';
+
+const IconeArr = [
+  { iconName: likeIcon, alt: 'likeIcon' },
+  { iconName: dislikeIcon, alt: 'likeIcon' },
+  { iconName: waveIcon, alt: 'waveIcon' },
+  { iconName: smileIcon, alt: 'smileIcon' },
+  { iconName: sadIcon, alt: 'sadIcon' },
+  { iconName: laughIcon, alt: 'laughIcon' },
+  { iconName: angryIcon, alt: 'angryIcon' },
+  { iconName: scaryIcon, alt: 'scaryIcon' },
+  { iconName: heartIcon, alt: 'heartIcon' },
+];
+
+export const Feedback: FC = () => {
+
+  //для первоначального отображения комментариев
+  const initfeedbackTextArr = [
+    ' 1Классные у тебя увлечения, я тоже играю в настолки, любимая игра — Эволюция. Люблю еще музыку слушать и отдыхать на природе. 🤣',
+    ' 2Классные у тебя увлечения, я тоже играю в настолки, любимая игра — Эволюция. Люблю еще музыку слушать и отдыхать на природе. 🤣',
+    ' 3Классные у тебя увлечения, я тоже играю в настолки, любимая игра — Эволюция. Люблю еще музыку слушать и отдыхать на природе. 🤣',
+  ];
+  const [inputValue, setInputValue] = useState<string>('');
+  const [feedbackTextArr, setFeedbackTextArr] =
+    useState<string[]>(initfeedbackTextArr);
+//тестовая функция для добавления комментариев
+  const addFeedback = () => {
+    setFeedbackTextArr([...feedbackTextArr, inputValue]);
+    setInputValue('');
+  };
+  return (
+    //блок коментариев
+    <div className={styles.feedbackModal}>
+      <div className={styles.feedbackTexts}>
+        {feedbackTextArr.map((text, i) => {
+          return (
+            <div className={styles.feedbackTextStyle} key={i}>
+              <p className={styles.feedbackText}>{text}</p>
+            </div>
+          );
+        })}
+      </div>
+      {/* блок ввода комментариев */}
+      <input
+        value={inputValue}
+        className={styles.feedbackInput}
+        placeholder="Обратная связь"
+        onChange={(e) => setInputValue(e.target.value)}
+        onMouseLeave={addFeedback}
+      ></input>
+      {/* блок эмоджи */}
+      <div className={styles.feedbackIcons}>
+        {IconeArr?.map((icon, index) => (
+          <div className={styles.feedbackIcon} key={index}>
+            <img src={icon.iconName} alt={icon.alt} />
+            <p className={styles.feedbackIconCount}>{'99+'}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
