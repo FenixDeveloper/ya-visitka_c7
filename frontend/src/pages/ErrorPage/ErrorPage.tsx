fd2ae035b385c8f1c9e17c297980e2fc9e490764
SmthOfValue <bik00014@gmail.com>
2023-04-03 21:58:34 +0300

    added eslint formatting on save

--- frontend/src/pages/ErrorPage/ErrorPage.tsx
+++ frontend/src/pages/ErrorPage/ErrorPage.tsx
@@ -1,5 +1,5 @@
 import React, { FC } from 'react';
-import { useRouteError } from "react-router-dom";
+import { useRouteError } from 'react-router-dom';
 import ErrorPageStyles from './ErrorPage.module.css';
 
 export const ErrorPage: FC = () => {
