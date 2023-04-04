15bc3b342954e4b43a22b44372103fd19150228a
Anton Aduisky <a.mltw@yandex.ru>
2023-04-04 16:25:47 +0300

    feat: app layout

--- frontend/src/components/App/App.tsx
+++ frontend/src/components/App/App.tsx
@@ -1,4 +1,5 @@
 import React, { FC } from 'react';
+import styles from './App.module.scss';
 import { RouterProvider } from 'react-router-dom';
 import Footer from '../Footer/Footer';
 import Header from '../Header/Header';
@@ -7,13 +8,13 @@ import { router } from '../../router/router';
 
 const App: FC = () => {
   return (
-    <>
+    <div className={styles.app}>
       <Header user={EXAMPLE_CURRENT_USER} />
       <main>
         <RouterProvider router={router} />
       </main>
       <Footer />
-    </>
+    </div>
   );
 };
 
