dc6f1ae8bb91ff3c3faa4f144d8e092f03681c04
Karkachevich <karkachevichtv@yandex.ru>
2023-04-05 09:45:31 +0500

    update jwt.strategy

--- backend/src/strategy/jwt.strategy.ts
+++ backend/src/strategy/jwt.strategy.ts
@@ -1,27 +1,32 @@
 import User from '../models/User';
+import passport from 'passport';
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
+const curatorList = ['curator_1@yandex.ru', 'curator_2@yandex.ru'];
 
 const opts = {
   jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
   secretOrKey: 'secret',
 }
 
-export const jwtStrategy = new JwtStrategy(opts, function(jwt_payload: any, done: any) {
-  console.log('payload received', jwt_payload);
 
-    User.findOne({id: jwt_payload.sub}, function(err: any, user: any) {
-      if (err) {
-          return done(err, false);
+export const jwtStrategy = new JwtStrategy(opts, function(jwt_payload: any, done: any) {
+ const email = jwt_payload.email;
+
+ const student = User.findOne({email});
+ const curator = curatorList.includes(email);
+
+ if (student) {
+    return done(null, student);
+  }
+ if (curator) {
+     return done(null, jwt_payload);
+    } else {
+            return done(null, false);
       }
-      if (user) {
-          return done(null, user);
-      } else {
-          return done(null, false);
-          // or you could create a new account
-      }
-    });
 
 });
+
+export const authenticate = passport.authenticate('jwt', {session: false});
 
 
