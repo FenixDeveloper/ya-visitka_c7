c81feeca431ee978b01c90fe8229af5db3463f0d
Артём Руденко <arudenko@elnis.ru>
2023-04-06 09:46:02 +0700

    add + style LoginPage

--- frontend/src/pages/LoginPage/LoginPage.tsx
+++ frontend/src/pages/LoginPage/LoginPage.tsx
@@ -1,12 +1,36 @@
-import React, { FC } from 'react';
-import LoginPageStyles from './LoginPage.module.css';
+import React, { FC, useEffect, useState } from 'react';
+import { Button } from '../../components/Button/Button';
+import LoginPageStyles from './LoginPage.module.scss';
 
 export const LoginPage: FC = () => {
+  const [windowWidth, setWindowWidth] = useState<number>(window.innerWidth);
+
+  useEffect(() => {
+    function updateWindowWidth() {
+      setWindowWidth(window.innerWidth)
+    }
+
+    window.addEventListener('resize', updateWindowWidth);
+
+    return () => window.removeEventListener('resize', updateWindowWidth);
+  }, [windowWidth])
+
   return (
-    <>
-      <h1>
-        Страница авторизации
+    <section className={LoginPageStyles.loginPage}>
+      <h1 className={LoginPageStyles.loginPage__title}>
+            С кем я учусь?
       </h1>
-    </>
+      <a className={LoginPageStyles.loginPage__buttonBox}
+        href='https://oauth.yandex.ru/authorize?response_type=code&client_id=6588f39ea0274d599d3c60fb10c53556'
+      >
+        <Button size={windowWidth <= 576 ? 'medium' : 'large'} 
+          htmlType='button'
+        >
+          <p className={LoginPageStyles.loginPage__buttonText}>
+              Войти с Яндекс ID
+          </p>
+        </Button>
+      </a>
+    </section>
   );
 }
