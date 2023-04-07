24e359465db4b0f5c75c9d40bd0a541a4cc71948
Артём Руденко <arudenko@elnis.ru>
2023-04-07 10:32:27 +0700

    add const TELEGRAM_URL

--- frontend/src/utils/constants.ts
+++ frontend/src/utils/constants.ts
@@ -1,6 +1,6 @@
 export const EXAMPLE_CURRENT_USER: { name: string, avatar?: string } = {
   name: 'Константин Константинович',
-  avatar: 'https://avatars.githubusercontent.com/u/85547727?v=4',
+  avatar: 'https://s0.rbk.ru/v6_top_pics/media/img/4/99/756723917919994.webp',
 };
 
 export const EXAMPLE_DEFAUT_ARR = ['Петровск (Саратовская область)', 'Петровск-Забайкальский (Забайкальский край)',
@@ -11,6 +11,7 @@ export const MONTHS = ['Январь', 'Февраль', 'Март', 'Апрел
 
 export const BASE_URL = 'https://visitki.practicum-team.ru/api';
 export const GITHUB_URL = 'https://api.github.com/';
+export const TELEGRAM_URL = 'https://tlgg.ru/test'
 export const DEFAULT_PAGE = 'DEFAULT_PAGE';
 export const ROMANTIC_PAGE = 'ROMANTIC_PAGE';
 export const COCKY_PAGE = 'COCKY_PAGE';
