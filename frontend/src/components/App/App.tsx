2efc23708051f5ab06a662237f3d72443d039a73
SmthOfValue <bik00014@gmail.com>
2023-04-08 15:38:32 +0300

    refactor added state to App

--- frontend/src/components/App/App.tsx
+++ frontend/src/components/App/App.tsx
@@ -1,19 +1,19 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import { RouterProvider } from 'react-router-dom';
 import { EXAMPLE_CURRENT_USER } from '../../utils/constants';
 import { router } from '../../router/router';
 import { ProfileContext } from '../../services/profileContext';
 
 const App: FC = () => {
-  const profileState = {
+  const [profileState, setProfileState] = useState({
     select: 'Петровск (Саратовская область)',
     date: new Date(1990, 0, 7),
     file: '',
     user: EXAMPLE_CURRENT_USER,
-  };
+  });
 
   return (
-    <ProfileContext.Provider value={profileState}>
+    <ProfileContext.Provider value={[profileState, setProfileState]}>
       <RouterProvider router={router} />
     </ProfileContext.Provider>
   );
