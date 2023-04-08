94841b8649a3e7d8da18afc944efe3a29b88ecaf
Genia V <evgenia.verhovtseva@yandex.ru>
2023-04-08 16:53:08 +0300

    added validation to comments routes

--- backend/src/routes/comments.ts
+++ backend/src/routes/comments.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import { getComments, deleteComment } from '../controllers/comments';
+import { getCommentsValidator, deleteCommentValidator } from '../helpers/validators';
 
 const router = Router();
 
-router.get('/', getComments);
+router.get('/', getComments, getCommentsValidator);
 
-router.delete('/:id', deleteComment);
+router.delete('/:id', deleteComment, deleteCommentValidator);
 
 export default router;
