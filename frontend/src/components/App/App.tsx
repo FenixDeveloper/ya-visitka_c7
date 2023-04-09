92128cb77f3ba800d81fee612f7aef140df9fdf2
Artrudjob <Artrudjob@yandex.ru>
2023-04-09 15:14:02 +0300

    refactoring styles: remove vertical scroll

--- frontend/src/components/App/App.tsx
+++ frontend/src/components/App/App.tsx
@@ -1,4 +1,5 @@
 import React, { FC } from "react";
+import styles from "./App.module.css";
 import { RouterProvider } from "react-router-dom";
 import Footer from "../Footer/Footer";
 import Header from "../Header/Header";
@@ -7,13 +8,13 @@ import { router } from "../../router/router";
 
 const App: FC = () => {
   return (
-    <>
+    <div className={styles.App}>
       <Header user={testUser} />
-      <main>
+      <main className={styles.App__main}>
         <RouterProvider router={router} />
       </main>
       <Footer />
-    </>
+    </div>
   );
 };
 
