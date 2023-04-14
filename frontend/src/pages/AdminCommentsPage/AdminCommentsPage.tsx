d5469e8abb5dca9fa01b623fee8ba4c3f507f159
isvakulenko <ivan-vakulenko@yandex.ru>
2023-04-14 07:03:26 +0300

    Add input filter, render Comments

--- frontend/src/pages/AdminCommentsPage/AdminCommentsPage.tsx
+++ frontend/src/pages/AdminCommentsPage/AdminCommentsPage.tsx
@@ -1,9 +1,39 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import styles from './AdminCommentsPage.module.scss';
 import { NavLink } from 'react-router-dom';
 import { Comments } from '../../components/Comments/Comments';
+import { EXAMPLE_COMMENTS_ARRAY } from '../../utils/constants';
 
 export const AdminCommentsPage: FC = () => {
+  const [query, setQuery] = useState('');
+  const [commentsData, setCommentsData] = useState(EXAMPLE_COMMENTS_ARRAY);
+  const [filterResult, setFilterResult] = useState(commentsData);
+
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const results = commentsData.filter(comment => {
+      if (e.target.value === '') return commentsData
+      return (comment.cohort.toLowerCase().includes(e.target.value.toLowerCase()) 
+         || comment.sender.toLowerCase().includes(e.target.value.toLowerCase()))
+    });
+    setFilterResult(results);
+    setQuery(e.target.value);
+  }
+
+  const comments = filterResult.map((comment, i) => {
+    return (
+      <Comments
+        key={i}
+        cohort={comment.cohort}
+        date={comment.date}
+        sender={comment.sender}
+        recipient={comment.recipient}
+        from={comment.from}
+        text={comment.text}
+      />
+    );
+  });
+
   return (
     <>
       <h1>Административная страница: модерирование комментариев</h1>
@@ -12,21 +42,37 @@ export const AdminCommentsPage: FC = () => {
           <NavLink
             to="/admin/users"
             className={({ isActive }) =>
-              isActive ? `${styles.link_active}` : `${styles.link_no_active}`
+              isActive ? styles.link + ' ' + styles.link_active : styles.link
             }
           >
-            СТУДЕНТЫ
+            студенты
           </NavLink>
           <NavLink
             to="/admin"
             className={({ isActive }) =>
-              isActive ? `${styles.link_active}` : `${styles.link_no_active}`
+              isActive ? styles.link + ' ' + styles.link_active : styles.link
             }
           >
-            КОММЕНТАРИИ
+            комментарии
           </NavLink>
         </div>
-        <Comments />
+        <p className={styles.filter_text}>Фильтровать</p>
+        <input
+          className={styles.filter_input}
+          type="text"
+          onChange={handleChange}
+          value={query}
+          placeholder="По имени или фамилии или почте или номеру когорты (введите любой из этих параметров)"
+        />
+        <div className={styles.tableHead}>
+          <p className={styles.tableHeadColumn}>Когорта</p>
+          <p className={styles.tableHeadColumn}>Дата</p>
+          <p className={styles.tableHeadColumn}>Отправитель</p>
+          <p className={styles.tableHeadColumn}>Получатель</p>
+          <p className={styles.tableHeadColumn}>Откуда комментарий</p>
+          <p className={styles.tableHeadColumn}>Текст комментария</p>
+        </div>
+        <ul className={styles.comments_list}>{comments}</ul>
       </section>
     </>
   );
