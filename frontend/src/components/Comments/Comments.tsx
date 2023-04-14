1d486d5b0d139383f3e5303b2e14badf65a8c839
isvakulenko <ivan-vakulenko@yandex.ru>
2023-04-14 07:01:58 +0300

    Add props for Comments, add delete button

--- frontend/src/components/Comments/Comments.tsx
+++ frontend/src/components/Comments/Comments.tsx
@@ -1,32 +1,39 @@
-import React, { FC, useState } from 'react';
+import React, { FC, SyntheticEvent } from 'react';
 import styles from './Comments.module.scss';
+import trash from '../../assets/icons/trash.svg';
 
-export const Comments: FC = () => {
+
+type TComments = {
+  cohort: string;
+  date: string;
+  sender: string;
+  recipient: string;
+  from: string;
+  text: string;
+  onClick?: (() => void) | ((e: SyntheticEvent) => void);
+};
+
+export const Comments: FC<TComments> = ({
+  cohort,
+  date,
+  sender,
+  recipient,
+  from,
+  text,
+  onClick
+}) => {
   return (
     <>
-      <div className={styles.tableHead}>
-        <p className={styles.tableHeadColumn}>Когорта</p>
-        <p className={styles.tableHeadColumn}>Дата</p>
-        <p className={styles.tableHeadColumn}>Отправитель</p>
-        <p className={styles.tableHeadColumn}>Получатель</p>
-        <p className={styles.tableHeadColumn}>Откуда комментарий</p>
-        <p className={styles.tableHeadColumn}>Текст комментария</p>
-      </div>
       <div className={styles.tableRow}>
-        <p
-          className={styles.tableData}
-        >
-          07
-        </p>
-        <p className={styles.tableData}>12.04.2023</p>
-        <p className={styles.tableData}>Дмитрий Степанов</p>
-        <p className={styles.tableData}>Виктория Тихомирова</p>
-        <p className={styles.tableData}>из блока Увлечения</p>
-        <p className={styles.tableData}>
-          Классные у тебя увлечения, я тоже играю в настолки, любимая игра —
-          Эволюция. Люблю еще слушать музыку, ходить в кино, общаться с
-          друзьями. Ну и учиться в Практикуме.{' '}
-        </p>
+        <p className={styles.tableData}>{cohort}</p>
+        <p className={styles.tableData}>{date}</p>
+        <p className={styles.tableData}>{sender}</p>
+        <p className={styles.tableData}>{recipient}</p>
+        <p className={styles.tableData}>{from}</p>
+        <p className={styles.tableData}>{text}</p>
+        <button className={styles.deleteButton}onClick={onClick}>
+          <img src={trash} alt='Нажать для удаления' />
+        </button>
       </div>
     </>
   );
