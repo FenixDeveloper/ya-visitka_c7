897108d8459f3487c3adf49ba35aede9cecdae80
isvakulenko <ivan-vakulenko@yandex.ru>
2023-04-18 22:39:47 +0300

    Fix NavLink for comments, refactor dateFromObjectId, add filterResultTest

--- frontend/src/pages/AdminCommentsPage/AdminCommentsPage.tsx
+++ frontend/src/pages/AdminCommentsPage/AdminCommentsPage.tsx
@@ -3,17 +3,16 @@ import styles from './AdminCommentsPage.module.scss';
 import { NavLink } from 'react-router-dom';
 import { Comment } from '../../components/Comment/Comment';
 import { ICommentData } from '../../services/types/data';
-import api from '../../utils/api-config';
+import { api } from '../../utils/api-config';
 
 export const AdminCommentsPage: FC = () => {
   const [query, setQuery] = useState('');
   const [commentsData, setCommentsData] = useState<ICommentData[]>([]);
   const [filterResult, setFilterResult] = useState<ICommentData[]>([]);
 
-  const bearerToken =
-    'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOm51bGwsInJvbGUiOiJjdXJhdG9yIiwiZW1haWwiOiJpdmFuLXZha3VsZW5rb0B5YW5kZXgucnUiLCJpYXQiOjE2ODE3NTYzOTcsImV4cCI6MTY4MjM2MTE5N30.mbkRf-HOLjwSpRENNHJBWrcucnHyt6qKpTU5uFr5SpM';
+  
   const fetchComments = async () => {
-    await api.getComments(bearerToken, 0, 20, '').then((data) => {
+    await api.getComments(0, 20, '').then((data) => {
       setCommentsData(data.items);
       setFilterResult(data.items);
     });
@@ -44,17 +43,130 @@ export const AdminCommentsPage: FC = () => {
   };
 
   const handleDeleteClick = async (id: string) => {
-    await api.deleteComments(bearerToken, id);
+    await api.deleteComments(id);
     setCommentsData([...commentsData.filter((el) => el._id !== id)]);
     setFilterResult([...filterResult.filter((el) => el._id !== id)]);
   };
 
+  //достанем дату из _id базы MongoDB https://steveridout.com/mongo-object-time/
+
   const dateFromObjectId = (objectId: string) => {
     const commentDate = new Date(parseInt(objectId.substring(0, 8), 16) * 1000);
-    return `${commentDate.getDate()}.${commentDate.getMonth()}.${commentDate.getFullYear()}`;
+    return `${
+      +commentDate.getDate() < 10
+        ? '0' + commentDate.getDate()
+        : commentDate.getDate()
+    }.${
+      +commentDate.getMonth() < 10
+        ? '0' + commentDate.getMonth()
+        : commentDate.getMonth()
+    }.${commentDate.getFullYear()}`;
   };
-
-  const comments = filterResult.map((comment) => {
+  const filterResultTest = [
+    {
+      _id: '81ffe58b6fe8dac5328b4f5e',
+      from: {
+        _id: '88fd917fb9ce1ebbacf35dcb',
+        name: 'Brendan Fadel',
+        email: 'Buster_Murphy@hotmail.com',
+        cohort: 'web+16',
+      },
+      target: 'hobby',
+      text: 'Fugit sint occaecati dicta unde eaque.',
+      to: {
+        _id: 'a67fdbbc9e4a2f7d962c795c',
+        name: 'Gordon Anderson',
+        email: 'Kayli72@hotmail.com',
+        cohort: 'web+16',
+      },
+    },
+    {
+      _id: '0c742efe72f6bc7a140863d6',
+      from: {
+        _id: '88fd917fb9ce1ebbacf35dcb',
+        name: 'Brendan Fadel',
+        email: 'Buster_Murphy@hotmail.com',
+        cohort: 'web+16',
+      },
+      target: 'edu',
+      text: 'Quas recusandae illo temporibus saepe repudiandae optio eligendi quas pariatur.',
+      to: {
+        _id: 'a67fdbbc9e4a2f7d962c795c',
+        name: 'Gordon Anderson',
+        email: 'Kayli72@hotmail.com',
+        cohort: 'web+16',
+      },
+    },
+    {
+      _id: '47addcbfe138dbdfbecefcd5',
+      from: {
+        _id: '88fd917fb9ce1ebbacf35dcb',
+        name: 'Brendan Fadel',
+        email: 'Buster_Murphy@hotmail.com',
+        cohort: 'web+16',
+      },
+      target: 'status',
+      text: 'Классные у тебя увлечения, я тоже играю в настолки, любимая игра — Эволюция. Люблю еще слушать музыку, ходить в кино, общаться с друзьями. Ну и учиться в Практикуме.',
+      to: {
+        _id: 'a67fdbbc9e4a2f7d962c795c',
+        name: 'Gordon Anderson',
+        email: 'Kayli72@hotmail.com',
+        cohort: 'web+16',
+      },
+    },
+    {
+      _id: '61c4da18bc63b512fffbe70f',
+      from: {
+        _id: '88fd917fb9ce1ebbacf35dcb',
+        name: 'Brendan Fadel',
+        email: 'Buster_Murphy@hotmail.com',
+        cohort: 'web+16',
+      },
+      target: 'job',
+      text: 'Dicta placeat voluptates quaerat odio neque minima.78555555555555555555555555555555',
+      to: {
+        _id: 'a67fdbbc9e4a2f7d962c795c',
+        name: 'Gordon Anderson',
+        email: 'Kayli72@hotmail.com',
+        cohort: 'web+16',
+      },
+    },
+    {
+      _id: 'd45e2dbcd98fce99bfaeef61',
+      from: {
+        _id: '88fd917fb9ce1ebbacf35dcb',
+        name: 'Brendan Fadel',
+        email: 'Buster_Murphy@hotmail.com',
+        cohort: 'web+16',
+      },
+      target: 'quote',
+      text: 'Ipsa amet autem cum.',
+      to: {
+        _id: 'a67fdbbc9e4a2f7d962c795c',
+        name: 'Gordon Anderson',
+        email: 'Kayli72@hotmail.com',
+        cohort: 'web+16',
+      },
+    },
+    {
+      _id: 'eff84876fbd0d069fde0d5fb',
+      from: {
+        _id: '88fd917fb9ce1ebbacf35dcb',
+        name: 'Brendan Fadel',
+        email: 'Buster_Murphy@hotmail.com',
+        cohort: 'web+16',
+      },
+      target: 'quote',
+      text: 'Tempora veritatis commodi est dicta doloremque animi dicta.',
+      to: {
+        _id: 'a67fdbbc9e4a2f7d962c795c',
+        name: 'Gordon Anderson',
+        email: 'Kayli72@hotmail.com',
+        cohort: 'web+16',
+      },
+    },
+  ];
+  const comments = filterResultTest.map((comment) => {
     return (
       <Comment
         key={comment._id}
@@ -82,7 +194,7 @@ export const AdminCommentsPage: FC = () => {
             студенты
           </NavLink>
           <NavLink
-            to="admin/comments"
+            to="/admin"
             className={({ isActive }) =>
               isActive ? styles.link + ' ' + styles.link_active : styles.link
             }
