e3b82ab895a8c8d5ecdbf5e09f20976baeb33501
Артём Руденко <arudenko@elnis.ru>
2023-04-19 09:06:14 +0700

    add NavLink

--- frontend/src/components/header/header.tsx
+++ frontend/src/components/header/header.tsx
@@ -2,11 +2,14 @@ import React, { FC } from 'react';
 import Login from '../login/login';
 import styles from './header.module.scss';
 import logo from '../../assets/icons/title-logo.svg';
+import { NavLink } from 'react-router-dom';
 
 const Header: FC = () => {
   return (
     <header className={styles.container}>
-      <img className={styles.container__title} src={logo} alt='VISITKI-logo' />
+      <NavLink to={'/'} className={styles.container__title}>
+        <img src={logo} alt='VISITKI-logo' />
+      </NavLink>
       <Login />
     </header>
   );
