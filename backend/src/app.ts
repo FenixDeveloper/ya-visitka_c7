028ad2bd8fa789d80e0deac02c75a03779463572
Cherepanov Kirill <tcherepanov.kir@gmail.com>
2023-04-20 22:03:39 +0300

    feat: add cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import mongoose from 'mongoose';
 import helmet from 'helmet';
 import 'isomorphic-fetch';
@@ -22,6 +23,7 @@ const limiter = rateLimit({
 const app = express();
 
 passport.use(jwtStrategy);
+app.use(cors());
 app.use(passport.initialize());
 app.use(
   mongoSanitize({
