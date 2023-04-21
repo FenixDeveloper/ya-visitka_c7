997c75fa00bc4cc2636f21b6a62303742adb398d
Артём Руденко <arudenko@elnis.ru>
2023-04-21 09:39:03 +0700

    add admin-page

---
+++ frontend/src/pages/admin-page/admin-page.tsx
@@ -0,0 +1,52 @@
+import React, { FC } from 'react';
+import styles from './admin-page.module.scss'
+import { EXAMPLE_USERS } from '../../utils/constants';
+
+export const AdminPage: FC = () => {
+
+  const changeToken = (token: string): void => {
+    console.log(token)
+  }
+    
+  const arrayUsers = EXAMPLE_USERS.map(item => {
+    if (item.role === 'user') {
+      return (
+        <button className={styles.admin__button} 
+          type='button' 
+          onClick={() => changeToken(item.token)} 
+          key={item._id}
+        >
+          <img className={styles.admin__avatar} src={item.avatar}></img>
+          <p className={styles.admin__text}>{item.name}</p>
+        </button>
+      )
+    }
+  })  
+
+  const arrayAdmins = EXAMPLE_USERS.map(item => {
+    if (item.role === 'admin') {
+      return (
+        <button 
+          className={styles.admin__button} 
+          type='button' 
+          onClick={() => changeToken(item.token)} 
+          key={item._id}
+        >
+          <img className={styles.admin__avatar} src={item.avatar}></img>
+          <p className={styles.admin__text}>{item.name}</p>
+        </button>
+      )
+    }
+  })
+
+  return (
+    <section className={styles.admin}>
+      <div className={styles.admin__container}>
+        <h2 className={styles.admin__title}>Администраторы:</h2>
+        {arrayAdmins}
+        <h2 className={styles.admin__title}>Пользователи:</h2>
+        {arrayUsers}
+      </div>
+    </section>
+  )
+}
